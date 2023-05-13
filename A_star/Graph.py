256d234ae333d26d0cc221b7969a36f8f6a4ed8b
Raid <raidrockstar040@gmail.com>
2023-05-13 01:48:11 +0100

    A * skeleton

---
+++ A_star/Graph.py
@@ -0,0 +1,34 @@
+import networkx as nx
+
+
+class CustomGraph:
+
+    """_summary_
+
+    Returns:
+        _dict_: returns the grph dict representation of a given graph_
+    """
+    __graph = {"A": [["B", 2], ["C", 3], 12, 8],  # h(n) to G ,h(n) to F
+               "B": [["A", 2], ["C", 4], ["D", 5], 10, 9],
+               "C": [["A", 3], ["B", 4], ["D", 3], ["F", 6], 8, 5],
+               "D": [["B", 5], ["C", 3], ["E", 2], 6, 4],
+               "E": [["G", 5], ["F", 3], ["D", 2], 4, 2],
+               "F": [["C", 6], ["E", 3], 6, 0],
+               "G": [["E", 5], 0, 6]
+               }
+
+    @staticmethod
+    def get_graph_dict():
+        return CustomGraph.__graph
+
+    """_summary_
+    Returns:
+        _nx_graph:The networkx Graph implmentation of the given dictionary
+    """
+    @staticmethod
+    def get_nx_graph():
+        G = nx.Graph()
+        G.add_nodes_from(CustomGraph.__graph.keys())
+        for k, v in CustomGraph.__graph.items():
+            G.add_edges_from([(k, v[t][0]) for t in range(len(v)-2)])
+        return G
