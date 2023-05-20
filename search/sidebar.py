a68f2b812af5f02e396019d413dbad2f9196561b
Abderrahim Rezki <abderrahim4cem@gmail.com>
2023-05-20 10:37:47 +0100

    modified the gui

---
+++ search/sidebar.py
@@ -0,0 +1,92 @@
+import tkinter as tk
+import networkx as nx
+
+class SideBar(tk.Frame):
+
+    def __init__(self, parent):
+
+        super().__init__(parent)
+
+        self.parent = parent
+
+        self.node_name_label = tk.Label(self, text="Node Name:")
+        self.node_name_label.grid(column=0, row=0)
+        self.node_name_entry = tk.Entry(self)
+        self.node_name_entry.grid(column=1, row=0)
+
+        self.node_value_label = tk.Label(self, text="Node Value:")
+        self.node_value_label.grid(column=0, row=1)
+        self.node_value_entry = tk.Entry(self)
+        self.node_value_entry.grid(column=1, row=1)
+
+        self.add_node_button = tk.Button(
+            self, text="Add Node", command=self.add_node)
+        self.add_node_button.grid(column=0, row=2, columnspan=2, sticky="nsew")
+
+        self.source_node_label = tk.Label(self, text="Source Node:")
+        self.source_node_label.grid(column=0, row=3)
+        self.source_node_entry = tk.Entry(self)
+        self.source_node_entry.grid(column=1, row=3)
+
+        self.target_node_label = tk.Label(self, text="Target Node:")
+        self.target_node_label.grid(column=0, row=4)
+        self.target_node_entry = tk.Entry(self)
+        self.target_node_entry.grid(column=1, row=4)
+
+        self.path_cost_label = tk.Label(self, text="Path Cost:")
+        self.path_cost_label.grid(column=0, row=5)
+        self.path_cost_entry = tk.Entry(self)
+        self.path_cost_entry.grid(column=1, row=5)
+
+        self.add_edge_button = tk.Button(
+            self, text="Add Edge", command=self.add_edge)
+        self.add_edge_button.grid(column=0, row=9, columnspan=2, sticky="nsew")
+
+        self.add_drop_down_menu()
+
+        self.apply_algorithm_button = tk.Button(
+            self, text="Apply Search Algorithm", command=self.apply_algorithm)
+        self.apply_algorithm_button.grid(column=0, row=11, columnspan=2, sticky="nsew")
+
+    def add_node(self):
+        node_name = self.node_name_entry.get()
+        node_value = self.node_value_entry.get()
+
+        if node_name not in self.parent.graph:
+            self.parent.graph.add_node(node_name, value=node_value)
+            print("Added node:", node_name, node_value)
+
+        self.parent._update_graph()
+
+    def add_edge(self):
+        source_node = self.source_node_entry.get()
+        target_node = self.target_node_entry.get()
+        path_cost = self.path_cost_entry.get()
+
+        self.parent.graph.add_edge(source_node, target_node, weight=path_cost)
+        print("Added edge:", source_node, "->", target_node)
+
+        self.parent._update_graph()
+
+    def add_drop_down_menu(self):
+        options = [
+            "Breadth First Search",
+            "Depth First Search",
+            "Uniform Cost Search",
+            "Depth Limited",
+            "Iterative Deepening Search",
+            "Greedy Best First Search",
+            "A* Star"
+        ]
+
+        clicked = tk.StringVar()
+
+        clicked.set("Breadth First Search")
+
+        drop = tk.OptionMenu(self, clicked, *options)
+        drop.grid(column=0, row=10, sticky="nsew", columnspan=2)
+
+    def apply_algorithm(self):
+        print(
+            nx.to_dict_of_dicts(self.parent.graph)
+        )
