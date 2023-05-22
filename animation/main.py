f5df2540334cab1e4f031295776fff44083edaed
Mohcen <hugoboy2311@gmail.com>
2023-05-22 08:15:15 +0200

    fixed terminal freezing issue

--- animation/main.py
+++ animation/main.py
@@ -1,5 +1,13 @@
 from gui import Window
 
+def on_close():
+    window.destroy()
+    window.quit()
+
+
 if __name__ == "__main__":
     window = Window()
+
+    window.protocol('WM_DELETE_WINDOW', on_close)
+    
     window.mainloop()
